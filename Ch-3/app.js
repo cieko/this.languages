736b4fc6df714eccbc6dcd5df0e70485b300e735
cieko <rahulsaxena2021@hotmail.com>
2023-10-28 22:40:48 +0530

    changes

--- Ch-3/app.js
+++ Ch-3/app.js
@@ -1,25 +1,33 @@
 const app = Vue.createApp({
   data() {
     return {
+      i: 0,
       counter: 0,
+      name: "",
     };
   },
   methods: {
-    add(i) {
+    add() {
       this.counter =
         this.counter == 100
           ? this.counter
-          : this.counter + i > 100
+          : this.counter + this.i > 100
           ? this.counter
-          : this.counter + i;
+          : this.counter + this.i;
     },
-    subtract(i) {
+    subtract() {
       this.counter =
         this.counter == 0
           ? this.counter
-          : this.counter - i < 0
+          : this.counter - this.i < 0
           ? this.counter
-          : this.counter - i;
+          : this.counter - this.i;
+    },
+    setName(e) {
+      this.name = e.target.value;
+    },
+    handleI(e) {
+      this.i = Number(e.target.value) ? Number(e.target.value) : 1;
     },
   },
 });
